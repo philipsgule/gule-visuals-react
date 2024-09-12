ace589949f344840e4095524de2ab5c796f1f70f
Philip Gule <philipsgule@gmail.com>
2024-09-12 14:55:57 +0200

    Lagt til litt tekst :D

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,7 @@ const About = () => {
   return (
     <div className="center-container">
       <h1 className="center-title">About</h1>
+      <p className="center-text">Creates content so you don't have to :)</p>
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,8 @@ const Contact = () => {
   return (
     <div className="center-container">
       <h1 className="center-title">Contact</h1>
+      <p className="center-text">Instagram @philipsgule</p>
+      <p className="center-text">mailto: philipsgule@gmail.com</p>
     </div>
   );
 };
